bc70719c634ef29f5208da5f275b7585838dfe81
sohag-aust <frustratedcoder96@gmail.com>
2020-07-09 18:05:05 +0600

    code.cpp added to dev3 branch

---
+++ code.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int sum(int a, int b){
+    return a+b;
+}
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    int _sum = sum(a,b);
+    cout<<"Sum is: "<<_sum<<endl;
+    return 0;
+}
