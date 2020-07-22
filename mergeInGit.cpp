0d2ca02064fd250068ef3eaa7854429a50789aad
Ashraful Islam <ashraful.islam@cramstack.com>
2020-07-22 13:03:16 +0600

    code mergeInGit.cpp added in origin/mergeBranch-1

---
+++ mergeInGit.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void reverse_name(string name){
+    if(name.size() == 0) return ;
+    cout<<name[name.size()-1];
+    reverse_name(name.erase(name.size()-1));
+}
+
+int main(){
+    cout<<"Enter your name: ";
+    string name;
+    cin>>name;
+
+    reverse_name(name);
+    cout<<endl;
+    return 0;
+}
